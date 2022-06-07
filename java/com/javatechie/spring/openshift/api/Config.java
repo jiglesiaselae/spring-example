56a5310ef3221336c5270f27cba4ef676ce4a121
jiglesiaselae <99651955+jiglesiaselae@users.noreply.github.com>
2022-06-07 16:41:06 +0200

    Add files via upload

--- java/com/javatechie/spring/openshift/api/Config.java
+++ java/com/javatechie/spring/openshift/api/Config.java
@@ -5,19 +5,20 @@ import java.io.Serializable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Configuration
 @ConfigurationProperties(prefix = "myconfig")
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Config implements Serializable {
 
     private String message = "a message that can be changed live";
+
+    public String getMessage() {
+	return message;
+    }
+
+    public void setMessage(String message) {
+	this.message = message;
+    }
 
     private static final long serialVersionUID = -8900976432592584351L;
 
