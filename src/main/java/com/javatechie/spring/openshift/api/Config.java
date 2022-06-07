7df6b0cd8417a07c870a5769aa37d90e1720d4a7
jiglesiaselae <99651955+jiglesiaselae@users.noreply.github.com>
2022-06-07 17:16:51 +0200

    Add files via upload

--- src/main/java/com/javatechie/spring/openshift/api/Config.java
+++ src/main/java/com/javatechie/spring/openshift/api/Config.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "myconfig")
+@ConfigurationProperties(prefix = "bean")
 
 public class Config implements Serializable {
 
