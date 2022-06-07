aaf2b4e1e75f1cbdb9f3cfe6f2380de29ee62025
jiglesiaselae <99651955+jiglesiaselae@users.noreply.github.com>
2022-06-07 16:41:57 +0200

    Add files via upload

---
+++ src/main/java/com/javatechie/spring/openshift/api/Config.java
@@ -0,0 +1,25 @@
+package com.javatechie.spring.openshift.api;
+
+import java.io.Serializable;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "myconfig")
+
+public class Config implements Serializable {
+
+    private String message = "a message that can be changed live";
+
+    public String getMessage() {
+	return message;
+    }
+
+    public void setMessage(String message) {
+	this.message = message;
+    }
+
+    private static final long serialVersionUID = -8900976432592584351L;
+
+}
--- src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
+++ src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
@@ -1,5 +1,6 @@
 package com.javatechie.spring.openshift.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringExampleApplication {
 
-	@GetMapping("/")
-	public String welcome() {
-		return "Welcome to java";
-	}
+    @Autowired
+    private Config config;
 
-	@GetMapping("/{input}")
-	public String congrats(@PathVariable String input) {
-		return "hi " + input + " Your application deployed successfully....";
-	}
+    @GetMapping("/")
+    public String welcome() {
+	System.out.println("The message is: " + config.getMessage());
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringExampleApplication.class, args);
-	}
+	return "Welcome to java.Message from config map:" + config.getMessage();
+    }
+
+    @GetMapping("/{input}")
+    public String congrats(@PathVariable String input) {
+	return "hi " + input + "  Your application deployed successfully....";
+
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(SpringExampleApplication.class, args);
+    }
 }
