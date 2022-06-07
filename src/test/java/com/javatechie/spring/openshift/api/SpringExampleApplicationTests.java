124ae155d049096b06773d09940d97650b5ce4b6
jiglesiaselae <99651955+jiglesiaselae@users.noreply.github.com>
2022-06-07 09:14:03 +0200

    Add files via upload

--- src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
+++ src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
@@ -1,16 +1,16 @@
 package com.javatechie.spring.openshift.api;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SpringExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
